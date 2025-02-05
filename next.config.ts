a74dc4dbea4c3250ca4fbbd951928fd5b92ce4b5
Tahir-Chandia <mt3523559@gmail.com>
2025-02-04 23:54:07 -0800

    change

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images:{
     domains:["cdn.sanity.io"]
-  }
+  },
+   eslint:{
+    ignoreDuringBuilds:true
+}
 };
 
 export default nextConfig;
